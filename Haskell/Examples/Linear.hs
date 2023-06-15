a66a181d0ffeacae1b5724314785514468dd78f0
cjbartsch <134946465+cjbartsch@users.noreply.github.com>
2023-06-15 13:33:17 -0500

    Minor Changes

--- Haskell/Examples/Linear.hs
+++ Haskell/Examples/Linear.hs
@@ -21,16 +21,11 @@ xt x0 t mu sigma h = do
 
 main :: IO ()
 main = do
-  g <- newStdGen
   let sigmas = [(0.2, Just "blue"), (0.4, Just "green"), (0.6, Just "magenta"), (0.8, Just "red")]
-      xts = map (\sigma -> sample' g (xt 1.0 1.0 1.0 (fst sigma) 200)) sigmas
-      str = concatMap (\sigma -> (getANSI (snd sigma)) ++ show (fst sigma) ++ resetCode ++ ", ") sigmas
 
-  putStrLn ("Solution X(t) on the interval [0,1], with X(0) = μ = 1, σ = [" 
-    ++ take (length str - 2) str ++ "]")
-  plotWith options { colors = map snd sigmas } xts
-  
-  let meanSolutions = map (\sigma -> averagePath $ take 10 $ sample g (xt 1.0 1.0 1.0 (fst sigma) 200)) sigmas
-  putStrLn ("Average of 10 sample paths X(t) on the interval [0,1], with X(0) = μ = 1, σ = [" 
-    ++ take (length str - 2) str ++ "]")
-  plotWith options { colors = map snd sigmas } meanSolutions
+  mapM_ (\sigma -> do
+    let xts = map (\seed -> sample' (mkStdGen seed) (xt 1.0 1.0 1.0 (fst sigma) 200)) [30, 40 .. 120]
+        meanSolution = averagePath xts
+    putStrLn ("X(t) on the interval [0,1], with X(0) = μ = 1, σ = " ++ show (fst sigma))
+    plotWith options {colors = replicate (length xts) (snd sigma) ++ [Just "yellow"], minY = Just 0} 
+      (xts ++ [meanSolution])) sigmas
