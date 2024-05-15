971f9def0786c8c1e72a15546255f5c3b795e349
cjbartsch <134946465+cjbartsch@users.noreply.github.com>
2024-05-15 17:07:57 -0500

    Update DRandom.hs

--- Haskell/DRandom.hs
+++ Haskell/DRandom.hs
@@ -11,9 +11,17 @@ module DRandom ( DRandom,
     normal,
     multivariateNormal,
     wiener,
+    eulerMaruyama,
+    milstein,
+    solveBy,
+    eulerMaruyama',
+    solveBy',
     brownianBridge,
     dFromIntegral,
     dFromBool,
+    dSplitAt,
+    nGens,
+    average,
     sample,
     sample',
     sampleMean,
@@ -26,7 +34,7 @@ module DRandom ( DRandom,
 
 import System.Random hiding (uniform, sample, random)
 import Control.Monad
-import Data.List (unfoldr, transpose)
+import Data.List (unfoldr, transpose, nub)
 import Numeric.LinearAlgebra
 
 import Class
@@ -67,6 +75,9 @@ instance Fractional (DRandom Double) where
   (/) = liftM2 (/)
   recip = liftM recip
   fromRational = pure . fromRational
+
+instance Show (DRandom Double) where
+  show (DRandom action) = show (action (mkStdGen 0))
 
 -- Define DRandom as an instance of Distribution.
 
@@ -111,6 +122,51 @@ instance Distribution DRandom where
         u2 = run random gen'
     in (mu + sigma * sqrt (-2.0 * log u1) * cos (2.0 * pi * u2))
 
+eulerMaruyama :: StdGen
+     -> (Double -> Double)
+     -> (Double -> Double)
+     -> Double
+     -> (Double, Double)
+     -> (Double, StdGen)
+eulerMaruyama gen f g x (t1, t2) = (x + dt * (f x) + dW * (g x), snd $ split gen)
+  where dt = t2 - t1
+        dW = run (normal 0 (sqrt dt)) gen
+
+milstein :: StdGen
+     -> (Double -> Double)
+     -> (Double -> Double)
+     -> Double
+     -> (Double, Double)
+     -> (Double, StdGen)
+milstein gen f g x (t1, t2) = (x + dt * (f x) + dW * (g x) + 0.5 * (g x) * g' * ((dW^2) - dt), snd $ split gen)
+  where dt = t2 - t1
+        dW = run (normal 0 (sqrt dt)) gen
+        g' = (g (x + dW) - g x) / dW
+
+-- Solve general SDE: dX(t) = f[X(t)] dt + g[X(t)] dW(t)
+solveBy method f g mesh x0 gen = map fst results
+  where 
+    results = scanl (\(x, gen') (t1, t2) -> method gen' f g x (t1, t2)) (x0, gen) intervals
+    intervals = zip mesh (tail mesh)
+
+eulerMaruyama' :: StdGen
+     -> (Double -> Double)
+     -> (Double -> Double)
+     -> Double
+     -> (Double, Double)
+     -> Double
+eulerMaruyama' gen f g x (t1, t2) = x + dt * (f x) + dW * (g x)
+  where dt = t2 - t1
+        dW = run (normal 0 (sqrt dt)) gen
+
+-- Solve general SDE: dX(t) = f[X(t)] dt + g[X(t)] dW(t)
+solveBy' method f g mesh x0 seed = results
+  where 
+    results = scanl (\x (gen, t) -> method gen f g x t) x0 gens
+    intervals = zip mesh (tail mesh)
+    gens = zip (nGens (length intervals) (mkStdGen seed)) intervals
+
+
 multivariateNormal :: [Double] -> Matrix Double -> DRandom [Double]
 multivariateNormal mu c = DRandom $ \gen ->
   let cholesky = chol (trustSym c)
@@ -119,10 +175,18 @@ multivariateNormal mu c = DRandom $ \gen ->
   in (zipWith (+) mu (toList ys))
 
 wiener :: Double -> Int -> DRandom [Double]
-wiener n h = DRandom $ \gen ->
-  let dt = n / fromIntegral h
-      steps = map (* (sqrt dt)) $ take h $ (sample gen (normal 0 1))
-  in (scanl (+) 0 steps)
+wiener t n = DRandom $ \gen ->
+  let dt = t / fromIntegral n
+      steps = take n $ sample gen (normal 0 (sqrt dt))
+      path = scanl (+) 0 steps
+  in path
+
+poissonProcess :: Double -> Double -> Int -> DRandom [Int]
+poissonProcess lambda t n = DRandom $ \gen ->
+  let dt = t / fromIntegral n
+      steps = take n $ sample gen (poisson (lambda * dt))
+      path = scanl (+) 0 steps
+  in path
 
 brownianBridge :: (Double, Double) -> (Double, Double) -> Int -> DRandom [Double]
 brownianBridge (_, x1) (t, yt) h = do
@@ -132,6 +196,11 @@ brownianBridge (_, x1) (t, yt) h = do
   return bridge
 
 -- UTILITIES --
+
+-- list of n rngs with no repeat seeds
+nGens n gen = map (\seed -> (mkStdGen seed)) $ take n $ nub $ randomRs (0, 10000) gen
+
+average xs = sum xs / fromIntegral (length xs)
 
 dFromIntegral :: DRandom Int -> DRandom Double
 dFromIntegral d = fmap fromIntegral d
@@ -174,6 +243,28 @@ reduce path = zipWith (+) (fst $ split' path) (snd $ split' path)
     split' = \list -> case list of
             [] -> ([], [])
             x:xs -> let (evens, odds) = split' xs in (x:odds, evens)
+
+-- split a DRandom [Double] at a given index
+dSplitAt :: Int -> DRandom [Double] -> DRandom ([Double], [Double])
+dSplitAt i dist = do
+  values <- dist
+  let (before, after) = splitAt i values
+  return (before, after)
+
+-- takes left riemann sum of a given process (discretized on a given interval)
+ito :: Num a => StdGen -> DRandom [a] -> a
+ito g process = 
+  let wt = sample' g process
+      reimann = zipWith (\w1 w2 -> w1 * (w2 - w1)) wt (tail wt)
+  in (sum reimann)
+
+-- takes midpoint riemann sum of a given process (discretized on a given interval)
+stratonovich :: Fractional a => StdGen -> DRandom [a] -> a
+stratonovich g process =
+  let wt = sample' g process
+      midpoints = zipWith (\w1 w2 -> 0.5 * (w1 + w2)) wt (tail wt)
+      reimann = zipWith (\w i -> w * (wt !! (i + 1)) - w * (wt !! i)) (midpoints) [0..(length wt - 1)]
+  in (sum reimann)
 
 -- (a, b) : interval
 -- h : step length
