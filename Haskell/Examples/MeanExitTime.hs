c2acb39c51b4e382a0423c2a05f20a2399a5b0ec
cjbartsch <134946465+cjbartsch@users.noreply.github.com>
2024-05-15 17:12:25 -0500

    Update MeanExitTime.hs

--- Haskell/Examples/MeanExitTime.hs
+++ Haskell/Examples/MeanExitTime.hs
@@ -5,88 +5,87 @@ module MeanExitTime where
 import DRandom
 import Chart
 
-import Data.List (transpose)
+import Data.List (nub, maximumBy, find)
+import Data.Ord (comparing)
+import System.IO
 import System.Random hiding (uniform, sample, random, next)
 import Graphics.Matplotlib as Mt
 
--- Given SDE dX(t) = μ * X(t)dt + σ * X(t)dW(t) and initial value X(0) = X_0 ϵ (a,b),
+-- Helper; takeWhile that includes the last value
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' p = foldr (\x ys -> if p x then x:ys else [x]) []
+
+-- Given SDE dX(t) = μ * X(t)ⁿ dt + σ * X(t)ᵐ dW(t) and initial value X(0) = X_0 ϵ (a,b),
 --   define T_{exit} as the first time that solution leaves the open interval (a,b)
 
 -- Assuming X_0 = x, consider E[T_{exit}(x)] = u(x), then u(x) satisfies the BVP:
---   (1/2)g²(x) * d²u/dx² + f(x) * du/dx = -1 for x ϵ (a,b), u(a) = u(b) = 0.
+--   (μ/γ) * x²ⁿ u'' + μ * xᵐ u' = -1 where = (2μ/σ²)
+-- for x >> 1, x ϵ (a,b), u(a) = u(b) = 0.
 
--- Plot solution to BVP for dX(t) = μ * X(t)dt + σ * X(t)dW(t).
--- Plot numerical simulation of mean exit time.
+-- Parameters 
+method = eulerMaruyama'
 
-xt :: Double -> Double -> Double -> Double -> Int -> DRandom [Double]
--- x0 : X(0)
--- t : end time
--- mu, sigma : μ, σ
--- h : number of steps
-xt x0 t mu sigma h = do 
-  wt <- wiener t h
-  let dt = [ 0*t/(fromIntegral h), 1*t/(fromIntegral h) .. t]
-      path = zipWith (\t w -> x0 * exp ((mu - 0.5 * sigma * sigma) * t + sigma * w)) dt wt
-  return path
+mu = 0.6 
+sigma = 0.5
+m = 1
+n = 1
 
-meanExitTime :: Floating a => a -> a -> a -> a -> a -> a
+f x = mu * x^m
+g x = sigma * x^n
+(a, b) = (2.0, 5.0)
+
+timeInterval = 100
+nSteps = 1000
+dt = timeInterval / nSteps
+mesh = [0.0, 0.0 + dt .. timeInterval]
+
+nICs = 150
+dx0 = (b - a) / fromIntegral nICs
+xs = [a, a + dx0 .. b]
+
+nTrials = 1000
+
+-- Generate the solution path of an SDE until it exits a certain interval (a, b) 
+solveUntilExit method f g mesh (a, b) x0 seed = zip mesh results
+  where 
+    results = takeWhile' (\x -> x > a && x < b) $ scanl (\x (seed, (t1, t2)) -> method seed f g x (t1, t2)) x0 gens
+    intervals = zip mesh (tail mesh)
+    gens = zip (nGens (length intervals) (mkStdGen seed)) intervals
+
+exitTime method f g mesh (a, b) x0 seed = fst $ last $ solveUntilExit method f g mesh (a, b) x0 seed
+
+numericalMET = zipWith (avgExitTime) xs icSeeds
+  where 
+    icSeeds = take (nICs + 1) $ nub $ randomRs (0, 10000) (mkStdGen 730)
+    trialSeeds s = take nTrials $ nub $ randomRs (0, 10000) (mkStdGen s)
+    avgExitTime x0 seed = average $ map (exitTime method f g mesh (a, b) x0) (trialSeeds seed)
+
+a1 :: Floating a => a -> a -> a -> a -> a -> a
 -- a, b : interval 
 -- mu, sigma : μ, σ
-meanExitTime x a b mu sigma =
+a1 a b mu sigma x = -- if (mu == 1 && sigma == 1) then -(log x)^2 + ((log a) + (log b))*(log x) - (log a)*(log b) else 
   (1 / ((0.5 * sigma * sigma) - mu)) *
   (log (x / a) -
    ((1 - ((x / a) ** (1 - (2 * mu / (sigma * sigma))))) /
     (1 - ((b / a) ** (1 - (2 * mu / (sigma * sigma)))))) *
    log (b / a))
 
+analyticalMET = map (a1 a b mu sigma) xs
 
-exitTime :: Double -> Double -> Double -> Double -> Int -> Double -> Double -> Int -> [Double]
--- x0 : X(0)
--- t : end time
--- mu, sigma : μ, σ
--- h : number of steps
--- a, b : interval 
--- sampleSize : number of sample paths
-exitTime x0 t mu sigma h a b sampleSize =
-  let paths = map (\seed -> sample' (mkStdGen seed) (xt x0 t mu sigma h)) [0 .. sampleSize]
-      exitTime' path =
-        case filter (\(_, x) -> x <= a || x >= b) (zip [0.0 .. fromIntegral h] path) of
-          [] -> t  -- no exit
-          ((time, _):_) -> time
-  in (map (/ fromIntegral h) $ map exitTime' paths)
-
-simulatedMeanExitTime :: Double -> Double -> Double -> Double -> Int -> Double -> Double -> Int -> Double
-simulatedMeanExitTime x0 t mu sigma h a b sampleSize = sum (exitTime x0 t mu sigma h a b sampleSize) / fromIntegral sampleSize
-
+errorAtMax :: [Double] -> [Double] -> Double
+errorAtMax analyticalMET numericalMET =
+    abs $ (snd analyticalMax) - (snd numericalAtMax)
+  where 
+    analyticalMax = maximumBy (comparing snd) $ zip xs analyticalMET
+    numericalAtMax = case find (\(x', _) -> x' == fst analyticalMax) pairs of
+        Just pair -> pair
+        Nothing   -> error "Invalid comparison"
+    pairs = zip xs numericalMET
+    
 
 plotMeanExitTime :: IO ()
-plotMeanExitTime = onscreen $ Mt.plot xs analytical @@ [o2 "label" "analytical"]
-  % Mt.plot xs numerical @@ [o2 "label" "numerical"]
+plotMeanExitTime = onscreen $ Mt.plot xs analyticalMET @@ [o2 "label" "analytical"]
+  % Mt.plot xs (numericalMET) @@ [o2 "label" "numerical"]
   % title ("Mean exit time of X(t) on the interval (" 
-    ++ show a ++ ", " ++ show b ++ "), with μ = " ++ show mu ++ ", σ = " ++ show sigma) @@ [o2 "fontsize" 11]
+    ++ show a ++ ", " ++ show b ++ ")") @@ [o2 "fontsize" 11]
   % legend @@ [o2 "loc" "upper right"]
-  where
-    a = 2.0; b = 5.0; mu = 1.0; sigma = 0.6
-    xs = [2, 1.005 .. 5]; h = 1000
-    analytical = map (\x -> meanExitTime x a b mu sigma) xs
-    numerical = map (\x0 -> simulatedMeanExitTime x0 1.0 mu sigma h a b 100) xs
- 
-plotError :: IO ()
-plotError = onscreen $ Mt.plot hs weakErr @@ [o2 "label" "weak error"]
-  % Mt.plot hs strongErr @@ [o2 "label" "strong error"]
-  % title ("Error with μ = " ++ show mu ++ ", σ = " ++ show sigma) @@ [o2 "fontsize" 11]
-  % legend @@ [o2 "loc" "upper right"]
-  % xlabel "h"
-  % ylabel "error"
-  where
-    a = 1.0; b = 2.0; mu = 1.0; sigma = 0.6
-    xs = [1, 1.005 .. 2]; hs = [400 .. 700]; sampleSize = 10
-    analytical = map (\x -> meanExitTime x a b mu sigma) xs
-    err path = zipWith (\a n -> abs $ a - n) analytical path
-
-    numericalMean = map (\h -> map (\x0 -> simulatedMeanExitTime x0 1.0 mu sigma h a b sampleSize) xs) hs
-    weakErr = map (\ys -> maximum $ err ys) numericalMean
-    
-    averageList lists = map (\xs -> sum xs / fromIntegral (length xs)) (transpose lists)
-    numerical = map (\h -> map (\x0 -> exitTime x0 1.0 mu sigma h a b sampleSize) xs) hs
-    strongErr = map ( \ys -> maximum $ averageList (map (\sample -> err sample) ys) ) numerical
